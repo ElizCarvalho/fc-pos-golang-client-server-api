a4d65f4ef13cf8be6d798c5291bba5f0ebc99534
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-09-21 12:45:03 -0300

    refactor: remove commented code from main server file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,19 +7,16 @@ import (
 )
 
 func main() {
-	// Configurar rota de health check
 	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("I am alive! Let's go!"))
 	})
 
-	// Configurar rota de cotacao
 	http.HandleFunc("/quote", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello! This is the /quote endpoint"))
 	})
 
-	// Iniciar servidor
 	fmt.Println("Server is running on port 9090")
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
